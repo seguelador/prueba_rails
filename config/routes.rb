6fe7a58a8449acfd3dd79f13ce00c5558184e0fa
seguelador <matias.seguel.estay@gmail.com>
2016-07-05 19:53:21 -0400

    batman-vs-superman page added to root

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,13 @@
 Rails.application.routes.draw do
   get 'pages/batman'
   get 'pages/superman'
+  get 'pages/batman-vs-superman' => 'pages#batman_vs_superman'
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
-  # root 'welcome#index'
+  root 'pages#batman-vs-superman'
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
